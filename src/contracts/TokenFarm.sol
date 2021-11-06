4274a5cbd540d3913c6c1d5bf8b4ce38b6d350ec
Rahul Patni <rahul20patni@gmail.com>
2021-11-05 19:22:09 -0700

    building staking

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -6,10 +6,39 @@ import "./R8IToken.sol";
 contract TokenFarm {
     string public name = "r8i token farm";
     R8IToken public r8iToken;
+    address public owner;
     DaiToken public daiToken;
+
+    address[] public stakers;
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
 
     constructor(R8IToken _r8iToken, DaiToken _daiToken) public {
         daiToken = _daiToken;
         r8iToken = _r8iToken;
+    }
+
+    function stakeTokens(uint256 _amount) public {
+        require(_amount > 0, "amount cannot be 0");
+
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    function issueTokens() public {
+        require(msg.sender == owner, "caller must be owner");
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint256 balance = stakingBalance[recipient];
+            if (balance > 0) {
+                r8iToken.transfer(recipient, balance);
+            }
+        }
     }
 }
--- test/testTokenFarm.js
+++ test/testTokenFarm.js
@@ -8,13 +8,80 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("TokenFarm", (accounts) => {
-  // write tests
+function tokens(n) {
+  return web3.utils.toWei(n, "Ether");
+}
+
+contract("TokenFarm", ([owner, investor]) => {
+  let daiToken, r8iToken, tokenFarm;
+
+  before(async () => {
+    daiToken = await DaiToken.new();
+    r8iToken = await R8IToken.new();
+    tokenFarm = await TokenFarm.new(r8iToken.address, daiToken.address);
+
+    await r8iToken.transfer(tokenFarm.address, tokens("1000000"));
+
+    await daiToken.transfer(investor, tokens("100"), { from: owner });
+  });
+
   describe("Mock Dai Deployment", async () => {
     it("has a name", async () => {
-      let daiToken = await DaiToken.new();
       const name = await daiToken.name();
       assert.equal(name, "Mock DAI Token");
+    });
+  });
+
+  describe("R8I Dai Deployment", async () => {
+    it("has a name", async () => {
+      const name = await r8iToken.name();
+      assert.equal(name, "R8I Token");
+    });
+  });
+
+  describe("Token Farm Deployment", async () => {
+    it("has a name", async () => {
+      const name = await tokenFarm.name();
+      assert.equal(name, "r8i token farm");
+    });
+
+    it("contract has tokens", async () => {
+      let balance = await r8iToken.balanceOf(tokenFarm.address);
+      assert.equal(balance.toString(), tokens("1000000"));
+    });
+  });
+
+  describe("Farming tokens", async () => {
+    it("rewards investors for staking mDai tokens", async () => {
+      let result;
+
+      result = await daiToken.balanceOf(investor);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "investor mock DAI balance correct before staking"
+      );
+      await daiToken.approve(tokenFarm.address, tokens("100"), {
+        from: investor,
+      });
+      await tokenFarm.stakeTokens(tokens("100"), { from: investor });
+
+      result = await daiToken.balanceOf(investor);
+      assert.equal(
+        result.toString(),
+        tokens("0"),
+        "investor has no tokens after staking"
+      );
+
+      result = await tokenFarm.stackingBalance(investor);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "investor has 100 dai staked"
+      );
+
+      result = await tokenFarm.isStaking(investor);
+      assert.equal(result.toString(), "true", "investor has 100 dai staked");
     });
   });
 });
